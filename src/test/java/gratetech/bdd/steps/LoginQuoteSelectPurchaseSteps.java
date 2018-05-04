1935f98fbd1ad7a9d382c0dd5079430941dce046
Marcus Catt <mc@marcuscatt.co.uk>
2018-05-04 14:47:25 +0100

    moved date utils to utils

--- src/test/java/gratetech/bdd/steps/LoginQuoteSelectPurchaseSteps.java
+++ src/test/java/gratetech/bdd/steps/LoginQuoteSelectPurchaseSteps.java
@@ -5,11 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import gratetech.bdd.commons.DateStamp;
 import gratetech.bdd.interfaces.IBookingValidationStrategy;
 import gratetech.bdd.models.TouristBooking;
 import gratetech.bdd.models.TouristBooking.PassengerType;
 import gratetech.bdd.steps.serenity.UserPurchaseSteps;
+import gratetech.bdd.utils.DateStamp;
 import gratetech.bdd.verifications.TouristBookingStrategy;
 import net.thucydides.core.annotations.Steps;
 
